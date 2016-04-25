25e4fd4671713e0bab0f4f259261694603ce3a41
Bracintosh <bracapellan@gmail.com>
2016-04-24 20:24:15 -0400

    Add validation to ensure names are specified when creating projects

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,10 +1,13 @@
 require "rails_helper"
 
 RSpec.feature "Users can create new projects" do
-  scenario "with valid attributes" do
-      visit "/"
 
-      click_link "New Project"
+  before do
+    visit "/"
+    click_link "New Project"
+  end
+
+  scenario "with valid attributes" do
 
       fill_in "Name", with: "Sublime Text 3"
       fill_in "Description", with: "A text editor for everyone"
@@ -12,11 +15,18 @@ RSpec.feature "Users can create new projects" do
 
       expect(page).to have_content "Project has been created."
 
-      
+
       project = Project.find_by(name: "Sublime Text 3")
       expect(page.current_url).to eq project_url(project)
       
       title = "Sublime Text 3 - Projects - Ticketee"
       expect(page).to have_title title
+  end
+
+  scenario "when providing invalid attributes" do
+    click_button "Create Project"
+    
+    expect(page).to have_content "Project has not been created."
+    expect(page).to have_content "Name can't be blank"
   end
 end
